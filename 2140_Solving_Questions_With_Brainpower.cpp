6c89d5c8b48979aa623cda6e6ce64ea943a7e0cf
Suraj Gupta <surajgupta7070031833@gmail.com>
2025-04-01 23:49:49 +0530

    DP Problems

---
+++ 2140_Solving_Questions_With_Brainpower.cpp
@@ -0,0 +1,89 @@
+/*
+Solving Questions With Brainpower - [Leetcode - 2140(Medium)]
+=============================================================
+You are given a 0-indexed 2D integer array questions where questions[i] = [pointsi, brainpoweri].
+
+The array describes the questions of an exam, where you have to process the questions in order (i.e., starting from question 0) and make a decision whether to solve or skip each question. Solving question i will earn you pointsi points but you will be unable to solve each of the next brainpoweri questions. If you skip question i, you get to make the decision on the next question.
+
+For example, given questions = [[3, 2], [4, 3], [4, 4], [2, 5]]:
+If question 0 is solved, you will earn 3 points but you will be unable to solve questions 1 and 2.
+If instead, question 0 is skipped and question 1 is solved, you will earn 4 points but you will be unable to solve questions 2 and 3.
+Return the maximum points you can earn for the exam.
+
+Example 1:
+
+Input: questions = [[3,2],[4,3],[4,4],[2,5]]
+Output: 5
+Explanation: The maximum points can be earned by solving questions 0 and 3.
+- Solve question 0: Earn 3 points, will be unable to solve the next 2 questions
+- Unable to solve questions 1 and 2
+- Solve question 3: Earn 2 points
+Total points earned: 3 + 2 = 5. There is no other way to earn 5 or more points.
+
+Example 2:
+
+Input: questions = [[1,1],[2,2],[3,3],[4,4],[5,5]]
+Output: 7
+Explanation: The maximum points can be earned by solving questions 1 and 4.
+- Skip question 0
+- Solve question 1: Earn 2 points, will be unable to solve the next 2 questions
+- Unable to solve questions 2 and 3
+- Solve question 4: Earn 5 points
+Total points earned: 2 + 5 = 7. There is no other way to earn 7 or more points.
+ 
+
+Constraints:
+
+1 <= questions.length <= 105
+questions[i].length == 2
+1 <= pointsi, brainpoweri <= 105
+*/ 
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// Using Memoization (Top-Down DP) to solve the problem
+long long mostPoints(vector<vector<int>>& questions){
+    int n = questions.size();
+    vector<long long> dp(n + 1, 0);// dp[i] is the max points we can get from the first i questions
+    for(int i = n - 1; i >= 0; i--){
+        dp[i] = max(dp[i + 1], questions[i][0] + dp[min(n, i + questions[i][1] + 1)]);
+    }
+    return dp[0];
+}
+
+/*
+long long mostPoints(vector<vector<int>> &questions){
+    int n = questions.size();
+    vector<long long> dp(n + 1, 0);
+    for (int i = n - 1; i >= 0; --i){
+        int points = questions[i][0];
+        int brainpower = questions[i][1];
+
+        // Solve the current question
+        long long solve = points;
+        if (i + brainpower + 1 <= n){
+            solve += dp[i + brainpower + 1];
+        }
+        // Skip the current question
+        long long skip = dp[i + 1];
+
+        // Update the maximum points
+        dp[i] = max(solve, skip);
+    }
+    return dp[0];
+}
+*/
+ 
+int main(){
+        
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    vector<vector<int>> questions = {{3,2},{4,3},{4,4},{2,5}};
+    cout << mostPoints(questions) << endl; // Output: 5
+        
+    return 0;
+}
