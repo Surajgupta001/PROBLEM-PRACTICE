c350151134d487a5152bf08922157c24131382e5
Suraj Gupta <surajgupta7070031833@gmail.com>
2025-04-08 19:31:20 +0530

    Adding Brute Force solutions

--- 3396_Minimum_Number_Operations_Make_Elements_Array_Distinct.cpp
+++ 3396_Minimum_Number_Operations_Make_Elements_Array_Distinct.cpp
@@ -66,8 +66,36 @@ We come to the final formula of `(i + 3) / 3`
 
 #include <iostream>
 #include <vector>
+#include <unordered_set>
 using namespace std;
 
+/*
+================ Brute FOrce ===================
+int n;
+bool check(vector<int> &nums, int start){
+    unordered_set<int> st;
+    for (int j = start; j < n; j++){
+        if (st.count(nums[j])){
+            return false;
+        }
+        st.insert(nums[j]);
+    }
+    return true;
+}
+int minimumOperations(vector<int> &nums){
+    n = nums.size();
+    int ops = 0;
+    for (int i = 0; i < n; i += 3){
+        if (check(nums, i) == true){
+            return ops;
+        }
+        ops++;
+    }
+    return ops;
+}
+*/ 
+
+// Optimized Approach - Greedy
 int minimumOperations(vector<int>& nums) {
     vector<int> map(nums.size() + 1, 0);
     for (int i = nums.size() - 1; i >= 0; i--){
