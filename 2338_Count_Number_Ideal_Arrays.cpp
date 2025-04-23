64fba93906903dff271217e9a721c210d9fec6e5
Suraj Gupta <surajgupta7070031833@gmail.com>
2025-04-23 15:22:04 +0530

    Adding and improving code

--- 2338_Count_Number_Ideal_Arrays.cpp
+++ 2338_Count_Number_Ideal_Arrays.cpp
@@ -46,11 +46,61 @@ Constraints:
 #include <vector>
 using namespace std;
 
+/*
+============ Using Recursion ===================
+int Mod = 1e9 + 7;
+
+int helper(int start, int count, int n, int maxVal){
+   if(count == n) return 1;
+
+   long long result = 0;
+   for(int multiplier = 1; multiplier <= maxVal; multiplier++){
+      result = (result + helper(start * multiplier, count + 1, n, maxVal)) % Mod;
+   }
+   return result;
+}
+
+int idealArrays(int n, int maxValue){
+   long long result = 0;
+
+   for(int startVal = 1; startVal <= maxValue; startVal++){
+      result = (result + helper(startVal, 1, n, maxValue)) % Mod;
+   }
+   return result;
+}
+*/ 
+
+/*
+============ Using Dynamic Programming ===================
+int Mod = 1e9 + 7;
+vector<vector<int>> dp;
+
+int helper(int start, int count, int n, int maxVal){
+   if(count == n) return 1;
+   if(dp[start][count] != -1) return dp[start][count];
+   long long result = 0;
+   for(int multiplier = 1; start*multiplier <= maxVal; multiplier++){
+      result = (result + helper(start * multiplier, count + 1, n, maxVal)) % Mod;
+   }
+   return dp[start][count] = result; // Store the result in dp table
+}
+
+int idealArrays(int n, int maxValue){
+   long long result = 0;
+   dp.resize(maxValue + 1, vector<int>(n + 1, -1)); // Initialize dp table
+   
+   for(int startVal = 1; startVal <= maxValue; startVal++){
+      result = (result + helper(startVal, 1, n, maxValue)) % Mod;
+   }
+   return result;
+}
+*/ 
+
+
+// Using Dynamic Programming with Memoization
 int M = 1e9 + 7;
 void findSets(int m, vector<int> &count, vector<vector<int>> &dp){
-   if (dp[m][1] != 0){
-      return;
-   }
+   if (dp[m][1] != 0) return;
 
    dp[m][1] = 1;
    count[1]++;
@@ -84,7 +134,7 @@ int findPower(long long a, long long b){
 
 int modularnCr(int n, int r, vector<long long> &fact){
    if (r < 0 || r > n) return 0;
-   
+
    long long b = (fact[r] * fact[n - r]) % M;
    return (fact[n] * findPower(b, M - 2)) % M;
 }
@@ -116,4 +166,14 @@ int idealArrays(int n, int maxVal){
    }
 
    return (int)result;
+}
+
+int main(){
+   int n = 2, maxValue = 5;
+   cout << idealArrays(n, maxValue) << endl; // Output: 10
+
+   n = 5, maxValue = 3;
+   cout << idealArrays(n, maxValue) << endl; // Output: 11
+
+   return 0;
 }
