b55e4617f0ee4cde6d2f93d76358bfd742383df7
Suraj Gupta <surajgupta7070031833@gmail.com>
2025-04-27 16:04:51 +0530

    Improving

--- 3392_Count_Subarrays_Length_Three_With_Condition.cpp
+++ 3392_Count_Subarrays_Length_Three_With_Condition.cpp
@@ -38,13 +38,16 @@ Constraints:
 using namespace std;
 
 int countSubarrays(vector<int>& nums) {
+    int n = nums.size();
     int count = 0;
-    for(int i = 1; i < nums.size() - 1; ++i) {
-        if (nums[i - 1] + nums[i + 1] == nums[i] / 2) {
+
+    for(int i = 1; i < n-1; i++) {
+        if(nums[i] == (nums[i-1] + nums[i+1])*2) {
             count++;
         }
     }
-    return count;   
+
+    return count;
 }
     
 int main(){
