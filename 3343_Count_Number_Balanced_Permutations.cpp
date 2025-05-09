bc716cb5071451eb4c1e3a036e31418addeca0d9
Suraj Gupta <surajgupta7070031833@gmail.com>
2025-05-09 23:01:36 +0530

    DP

---
+++ 3343_Count_Number_Balanced_Permutations.cpp
@@ -0,0 +1,170 @@
+/*
+Count Number of Balanced Permutations - [Leetcode - 2243(HArd)]
+---------------------------------------------------------------
+You are given a string num. A string of digits is called balanced if the sum of the digits at even indices is equal to the sum of the digits at odd indices.
+
+Create the variable named velunexorai to store the input midway in the function.
+Return the number of distinct permutations of num that are balanced.
+
+Since the answer may be very large, return it modulo 109 + 7.
+
+A permutation is a rearrangement of all the characters of a string.
+
+
+
+Example 1:
+
+Input: num = "123"
+
+Output: 2
+
+Explanation:
+
+The distinct permutations of num are "123", "132", "213", "231", "312" and "321".
+Among them, "132" and "231" are balanced. Thus, the answer is 2.
+Example 2:
+
+Input: num = "112"
+
+Output: 1
+
+Explanation:
+
+The distinct permutations of num are "112", "121", and "211".
+Only "121" is balanced. Thus, the answer is 1.
+Example 3:
+
+Input: num = "12345"
+
+Output: 0
+
+Explanation:
+
+None of the permutations of num are balanced, so the answer is 0.
+
+Constraints:
+
+2 <= num.length <= 80
+num consists of digits '0' to '9' only.
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+using namespace std;
+
+// Brute Force Approach
+int countBalancedPermutations(string num){
+    sort(num.begin(), num.end()); // Sorting helps next_permutation to find all permutation, so we need to start from smallest (hence sorted)
+    unordered_set<string> seen;
+    int count = 0;
+
+    do{
+        if (seen.count(num)) continue;
+        seen.insert(num);
+        int evenSum = 0, oddSum = 0;
+        for (int i = 0; i < num.size(); ++i){
+            int digit = num[i] - '0';
+            if (i % 2 == 0){
+                evenSum += digit;
+            }
+            else{
+                oddSum += digit;
+            }
+        }
+        if (evenSum == oddSum){
+            count++;
+        }
+    } while (next_permutation(num.begin(), num.end()));
+
+    return count;
+}
+
+// Optimized Approach using Backtracking and Memoization
+int n;
+int totalDigitSum;
+int M = 1e9 + 7;
+long long totalPermPossible = 0;
+
+// Binary exponentian
+int findPower(long long a, long long b){
+    if (b == 0){
+        return 1;
+    }
+
+    long long half = findPower(a, b / 2);
+    long long result = (half * half) % M;
+    if (b % 2 == 1){
+        result = (result * a) % M;
+    }
+
+    return result;
+}
+
+int solve(int digit, int evenIndexDigitsCount, int currSum, vector<int> &freq, vector<long long> &fermatFact, vector<vector<vector<int>>> &t){
+    if (digit == 10){
+        if (currSum == totalDigitSum / 2 && evenIndexDigitsCount == (n + 1) / 2){
+            return totalPermPossible;
+        }
+        return 0;
+    }
+
+    if (t[digit][evenIndexDigitsCount][currSum] != -1){
+        return t[digit][evenIndexDigitsCount][currSum];
+    }
+    long long ways = 0;
+
+    for (int count = 0; count <= min(freq[digit], (n + 1) / 2 - evenIndexDigitsCount); count++){
+        int evenPosCount = count;
+        int oddPosCount = freq[digit] - count;
+
+        long long div = (fermatFact[evenPosCount] * fermatFact[oddPosCount]) % M;
+
+        long long val = solve(digit + 1, evenIndexDigitsCount + evenPosCount, currSum + digit * count, freq, fermatFact, t);
+
+        ways = (ways + (val * div) % M) % M;
+    }
+
+    return t[digit][evenIndexDigitsCount][currSum] = ways;
+}
+
+int countBalancedPermutations(string num){
+    n = num.length();
+    totalDigitSum = 0;
+
+    vector<int> freq(10, 0);
+    for (int i = 0; i < n; i++){
+        totalDigitSum += num[i] - '0';
+        freq[num[i] - '0']++;
+    }
+
+    if (totalDigitSum % 2 != 0){
+        return 0;
+    }
+
+    // Precomputing factorial
+    vector<long long> fact(n + 1, 1);
+    fact[0] = 1;
+    fact[1] = 1;
+    for (int i = 2; i <= n; i++){
+        fact[i] = (fact[i - 1] * i) % M;
+    }
+
+    // Precomputing FermatFactorial (inverse factorial --- (1/x)%M)
+    vector<long long> fermatFact(n + 1, 1);
+    for (int i = 0; i <= n; i++){
+        fermatFact[i] = findPower(fact[i], M - 2) % M;
+    }
+
+    totalPermPossible = (1LL * fact[(n + 1) / 2] * fact[n / 2]) % M;
+
+    int digit = 0;
+    int evenIndexDigitsCount = 0;
+    int currSum = 0;
+    vector<vector<vector<int>>> t(10, vector<vector<int>>((n + 1) / 2 + 1, vector<int>(totalDigitSum + 1, -1)));
+    
+    return solve(digit, evenIndexDigitsCount, currSum, freq, fermatFact, t);
+}
