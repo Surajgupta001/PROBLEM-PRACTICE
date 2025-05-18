59a328e54af1d366b6bbe2970c31ec3239f4307f
Suraj Gupta <surajgupta7070031833@gmail.com>
2025-05-18 15:35:56 +0530

    Array Tricky problem 3pointer approach

--- 75_Sort_Colors.cpp
+++ 75_Sort_Colors.cpp
@@ -23,3 +23,45 @@ n == nums.length
 1 <= n <= 300
 nums[i] is either 0, 1, or 2.
 */
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void sortColors(vector<int> &nums){
+    int n = nums.size();
+
+    int i = 0;      // denote for 0
+    int j = 0;      // denote for 1
+    int k = n - 1;  // denote for 2
+
+    while (j <= k){
+        if (nums[j] == 1){
+            j++;
+        }
+        else if (nums[j] == 2){
+            swap(nums[j], nums[k]);
+            k--;
+        }
+        else{
+            swap(nums[j], nums[i]);
+            i++;
+            j++;
+        }
+    }
+}
+
+int main(){
+    
+    vector<int> nums = {2,0,2,1,1,0};
+    
+    sortColors(nums);
+    
+    for(int i=0; i<nums.size(); i++){
+        cout << nums[i] << " ";
+    }
+
+    return 0;
+}
