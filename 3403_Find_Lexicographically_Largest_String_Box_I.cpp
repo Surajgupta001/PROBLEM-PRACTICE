0219e4e142ac97cbb76b29e3502b088c6a5fd5db
Suraj Gupta <surajgupta7070031833@gmail.com>
2025-06-06 11:52:54 +0530

    Improving to O(n) time and O(1) space

--- 3403_Find_Lexicographically_Largest_String_Box_I.cpp
+++ 3403_Find_Lexicographically_Largest_String_Box_I.cpp
@@ -9,8 +9,6 @@ word is split into numFriends non-empty strings, such that no previous round has
 All the split words are put into a box.
 Find the lexicographically largest string from the box after all the rounds are finished.
 
- 
-
 Example 1:
 
 Input: word = "dbca", numFriends = 2
@@ -48,6 +46,7 @@ word consists only of lowercase English letters.
 #include <string>
 using namespace std;
 
+// O(n^2) 
 string answerString(string word, int numFriends) {
     int n = word.size();
 
@@ -64,6 +63,45 @@ string answerString(string word, int numFriends) {
     }
     return result;
 }
+
+int bestStartingPoint(string word, int n) {
+    int i = 0; // Best Starting point of the first substring
+    int j = 1; // Keep moving to find the best starting point of the second substring
+
+    while(j < n){
+        int k = 0; // Length of the common prefix between the two substrings starting at i and j
+        
+        while(i + k < n && j + k < n && word[i + k] == word[j + k]) { // Compare characters at the current position and the next position
+            k++; // Increment the length of the common prefix
+        }
+
+        if(j + k < n && (i + k >= n || word[j + k] > word[i + k])) {
+            // If the next character of the second substring is greater than the first substring
+            i = j; // Update the best starting point to the current position
+            j = j + 1; // Move to the next position
+        }
+        else{
+            // If the next character of the first substring is greater than or equal to the second substring
+            j = j + k + 1; // Move to the next position after the common prefix
+        }
+    }
+    return i; // Return the best starting point of the first substring
+}
+// Better Approach: O(n) and space complexity O(1)
+string answerString(string word, int numFriends) {
+    int n = word.size();
+
+    if(numFriends == 1) return word;
+
+    int i = bestStartingPoint(word, n);
+
+    int longestPossibleLength = n - (numFriends - 1);
+
+    int canTakenLength = min(longestPossibleLength, n - i);
+    
+    return word.substr(i, canTakenLength); // Return the substring starting from the best starting point with the maximum
+}
+
 
 int main() {
 
