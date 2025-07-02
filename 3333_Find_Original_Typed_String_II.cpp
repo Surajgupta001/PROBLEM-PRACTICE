1cea9cdc6ebf6eb79a25bbf3eb2e899517746c7d
Suraj Gupta <surajgupta7070031833@gmail.com>
2025-07-02 22:18:55 +0530

    Add implementations for Original Typed String I and II problems

---
+++ 3333_Find_Original_Typed_String_II.cpp
@@ -0,0 +1,114 @@
+/*
+Find the Original Typed String II - [Leetcode - 2099(Hard)]
+-------------------------------------------------------------
+Alice is attempting to type a specific string on her computer. However, she tends to be clumsy and may press a key for too long, resulting in a character being typed multiple times.
+
+You are given a string word, which represents the final output displayed on Alice's screen. You are also given a positive integer k.
+
+Return the total number of possible original strings that Alice might have intended to type, if she was trying to type a string of size at least k.
+
+Since the answer may be very large, return it modulo 109 + 7.
+
+ 
+
+Example 1:
+
+Input: word = "aabbccdd", k = 7
+
+Output: 5
+
+Explanation:
+
+The possible strings are: "aabbccdd", "aabbccd", "aabbcdd", "aabccdd", and "abbccdd".
+
+Example 2:
+
+Input: word = "aabbccdd", k = 8
+
+Output: 1
+
+Explanation:
+
+The only possible string is "aabbccdd".
+
+Example 3:
+
+Input: word = "aaabbb", k = 3
+
+Output: 8
+
+ 
+
+Constraints:
+
+1 <= word.length <= 5 * 105
+word consists only of lowercase English letters.
+1 <= k <= 2000
+*/ 
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+    int M = 1e9+7;
+
+    int possibleStringCount(string word, int k) {
+        if(k > word.length())
+            return 0;
+        
+        vector<int> freq;
+        int count = 1;
+        for(int i = 1; i < word.length(); i++) {
+            if(word[i] == word[i-1]) {
+                count++;
+            } else {
+                freq.push_back(count);
+                count = 1;
+            }
+        }
+        freq.push_back(count);
+
+        long long P = 1; //total possible strings
+        for(int &f : freq) {
+            P = (P * f) % M;
+        }
+        
+        if(freq.size() >= k) {
+            return P;
+        }
+        int n = freq.size();
+        
+        vector<vector<int>> t(n+1, vector<int>(k+1, 0));
+
+        for(int count = k-1; count >= 0; count--) {
+            t[n][count] = 1;
+        }
+
+        for(int i = n-1; i >= 0; i--) {
+
+            vector<int> prefix(k+1, 0);
+            for(int h = 1; h <= k; h++) {
+                prefix[h] = (prefix[h-1] + t[i+1][h-1]) % M;
+            }
+
+            for(int count = k-1; count >= 0; count--) {
+                
+                int l = count + 1;
+                int r = count + freq[i];
+
+                if(r+1 > k) {
+                    r = k-1;
+                }
+
+                if(l <= r) {
+                    t[i][count] = (prefix[r+1] - prefix[l] + M) % M;
+                }
+                
+            }
+        }
+
+        long long invalidCount = t[0][0];
+
+        return (P - invalidCount + M) % M;
+    }
