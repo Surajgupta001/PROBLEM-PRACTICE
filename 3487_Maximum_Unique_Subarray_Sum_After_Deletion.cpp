ee8d8df34dad3d234857a52cae9784c14588fd86
Suraj Gupta <surajgupta7070031833@gmail.com>
2025-07-25 20:57:36 +0530

    Easy one

---
+++ 3487_Maximum_Unique_Subarray_Sum_After_Deletion.cpp
@@ -0,0 +1,77 @@
+/*
+Maximum Unique Subarray Sum After Deletion - [LeetCode - Easy(3487)]
+---------------------------------------------------------------------
+You are given an integer array nums.
+
+You are allowed to delete any number of elements from nums without making it empty. After performing the deletions, select a subarray of nums such that:
+
+All elements in the subarray are unique.
+The sum of the elements in the subarray is maximized.
+Return the maximum sum of such a subarray.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5]
+
+Output: 15
+
+Explanation:
+
+Select the entire array without deleting any element to obtain the maximum sum.
+
+Example 2:
+
+Input: nums = [1,1,0,1,1]
+
+Output: 1
+
+Explanation:
+
+Delete the element nums[0] == 1, nums[1] == 1, nums[2] == 0, and nums[3] == 1. Select the entire array [1] to obtain the maximum sum.
+
+Example 3:
+
+Input: nums = [1,2,-1,-2,1,0,-1]
+
+Output: 3
+
+Explanation:
+
+Delete the elements nums[2] == -1 and nums[3] == -2, and select the subarray [2, 1] from [1, 2, 1, 0, -1] to obtain the maximum sum.
+
+Constraints:
+
+1 <= nums.length <= 100
+-100 <= nums[i] <= 100
+*/ 
+
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <climits>
+using namespace std;
+
+int maxSum(vector<int>& nums) {
+    unordered_set<int> sets;
+    
+    int sum = 0;
+    int maxNeg = INT_MIN;
+
+    for(auto &num : nums){
+        if(num <= 0){
+            maxNeg = max(maxNeg, num);
+        } else if(!sets.count(num)) {
+            sets.insert(num);
+            sum += num;
+        }
+    }
+    return sum > 0 ? sum : maxNeg;
+}
+
+int main() {
+    vector<int> nums = {1, 2, -1, -2, 1, 0, -1};
+    cout << "Maximum Unique Subarray Sum After Deletion: " << maxSum(nums) << endl;
+    return 0;
+}
