df82fb333f3c461590879b63c6bc978910c23b42
Suraj Gupta <surajgupta7070031833@gmail.com>
2025-09-02 23:49:55 +0530

    Good Problems

---
+++ 3025_Find_Number_Ways_Place_People_I.cpp
@@ -0,0 +1,99 @@
+/*
+Find the Number of Ways to Place People I - [Leetcode - 3025]
+---------------------------------------------------------------
+You are given a 2D array points of size n x 2 representing integer coordinates of some points on a 2D plane, where points[i] = [xi, yi].
+
+Count the number of pairs of points (A, B), where
+
+A is on the upper left side of B, and
+there are no other points in the rectangle (or line) they make (including the border).
+Return the count.
+
+Example 1:
+
+Input: points = [[1,1],[2,2],[3,3]]
+
+Output: 0
+
+Explanation:
+
+There is no way to choose A and B so A is on the upper left side of B.
+
+Example 2:
+
+Input: points = [[6,2],[4,4],[2,6]]
+
+Output: 2
+
+Explanation:
+
+The left one is the pair (points[1], points[0]), where points[1] is on the upper left side of points[0] and the rectangle is empty.
+The middle one is the pair (points[2], points[1]), same as the left one it is a valid pair.
+The right one is the pair (points[2], points[0]), where points[2] is on the upper left side of points[0], but points[1] is inside the rectangle so it's not a valid pair.
+Example 3:
+
+Input: points = [[3,1],[1,3],[1,1]]
+
+Output: 2
+
+Explanation:
+
+The left one is the pair (points[2], points[0]), where points[2] is on the upper left side of points[0] and there are no other points on the line they form. Note that it is a valid state when the two points form a line.
+The middle one is the pair (points[1], points[2]), it is a valid pair same as the left one.
+The right one is the pair (points[1], points[0]), it is not a valid pair as points[2] is on the border of the rectangle.
+
+Constraints:
+
+2 <= n <= 50
+points[i].length == 2
+0 <= points[i][0], points[i][1] <= 50
+All points[i] are distinct.
+ 
+*/
+
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <algorithm>
+using namespace std;
+
+int numberOfPairs(vector<vector<int>>& points) {
+    int n = points.size();
+    // Sort: x ascending, if x same then sort as y descending
+    auto lambda = [](vector<int>& point1, vector<int>& point2) {
+        if (point1[0] == point2[0]) {
+            return point1[1] > point2[1];
+        }
+        return point1[0] < point2[0];
+    };
+    sort(points.begin(), points.end(), lambda);
+    int result = 0;
+    for (int i = 0; i < n; i++) {
+        int x1 = points[i][0];
+        int y1 = points[i][1];   // upper left
+        int bestY = INT_MIN;
+        for (int j = i + 1; j < n; j++) {
+            int x2 = points[j][0];
+            int y2 = points[j][1];   // lower right
+            // Condition: (x2, y2) must be above (x1, y1)
+            if (y2 > y1) { //not lower right
+                continue;
+            }
+            if (y2 > bestY) {
+                result++;
+                bestY = y2;
+            }
+        }
+    }
+    return result;
+}
+
+int main() {
+    vector<vector<int>> points = {{1,1},{2,2},{3,3}};
+    cout << numberOfPairs(points) << endl;  // Output: 0
+    points = {{6,2},{4,4},{2,6}};
+    cout << numberOfPairs(points) << endl;  // Output: 2
+    points = {{3,1},{1,3},{1,1}};
+    cout << numberOfPairs(points) << endl;  // Output: 2
+    return 0;
+}
