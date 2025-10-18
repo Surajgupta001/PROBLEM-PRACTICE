790b091d273ab6bb98feab472967f47c0106bb75
Suraj Gupta <surajgupta7070031833@gmail.com>
2025-10-18 20:13:08 +0530

    Add solution for Maximum Number of Distinct Elements After Operations (Leetcode 3397)

---
+++ 3397_Maximum_Number_Distinct_Elements_After_Operations.cpp
@@ -0,0 +1,73 @@
+/*
+Maximum Number of Distinct Elements After Operations - [Leetcode - 3397(Medium)]
+----------------------------------------------------------------------------------
+You are given an integer array nums and an integer k.
+
+You are allowed to perform the following operation on each element of the array at most once:
+
+Add an integer in the range [-k, k] to the element.
+Return the maximum possible number of distinct elements in nums after performing the operations.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,2,3,3,4], k = 2
+
+Output: 6
+
+Explanation:
+
+nums changes to [-1, 0, 1, 2, 3, 4] after performing operations on the first four elements.
+
+Example 2:
+
+Input: nums = [4,4,4,4], k = 1
+
+Output: 3
+
+Explanation:
+
+By adding -1 to nums[0] and 1 to nums[1], nums changes to [3, 5, 4, 4].
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 109
+0 <= k <= 109
+*/ 
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int maxDistinctElements(vector<int>& nums, int k) {
+    int n = nums.size();
+    sort(begin(nums), end(nums));
+    int count = 0;
+    int prev = INT_MIN;
+    for(int i = 0; i < n; i++) {
+        int minVal = nums[i] - k;
+        if(prev < minVal) {
+            //nums[i] = nums[i]-k;
+            prev = minVal;
+            count++;
+        } else if(prev < nums[i] + k) {
+            //nums[i] = prev+1;
+            prev = prev+1;
+            count++;
+        }
+    }
+    return count;
+}
+
+int main () {
+    vector<int> nums = {1,2,2,3,3,4};
+    int k = 2;
+    cout << maxDistinctElements(nums, k) << endl;
+    return 0;
+}
