555ba7d84bde8d29510b05edf12586ad3be3260a
Suraj Gupta <surajgupta7070031833@gmail.com>
2025-10-22 18:03:56 +0530

    Adding one more solution of DIfference Array Technique

--- 3346_Maximum_Frequency_Element_After_Performing_Operations_I.cpp
+++ 3346_Maximum_Frequency_Element_After_Performing_Operations_I.cpp
@@ -47,6 +47,7 @@ Constraints:
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
 using namespace std;
 
 int maxFrequency(vector<int>& nums, int k, int numOperations) {
@@ -80,6 +81,41 @@ int maxFrequency(vector<int>& nums, int k, int numOperations) {
         int maxPossibleFrequency = targetCount + min(needToConvert, numOperations);
 
         result = max(result, maxPossibleFrequency);
+    }
+    
+    return result;
+}
+
+int maxFrequency(vector<int>& nums, int k, int numOperations) {
+    int maxVal = *max_element(begin(nums), end(nums)) + k;
+    
+    vector<int> diff(maxVal+2, 0);
+    
+    unordered_map<int, int> freq;
+    
+    for(int i = 0; i < nums.size(); i++) {
+        freq[nums[i]]++;
+        
+        int l = max(nums[i]-k, 0);
+        int r = min(nums[i]+k, maxVal);
+        
+        diff[l]++;
+        diff[r+1]--;
+    }
+
+    int result = 1;
+    
+    for(int target = 0; target <= maxVal; target++) {
+        
+        diff[target] += (target > 0 ? diff[target-1] : 0);
+        
+        int targetFreq     = freq[target];
+        
+        int needConversion = diff[target] - targetFreq;
+        
+        int maxPossibleFreq = min(needConversion, numOperations);
+        
+        result = max(result, targetFreq + maxPossibleFreq);
     }
     
     return result;
