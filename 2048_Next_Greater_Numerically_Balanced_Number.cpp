d39391f909af7b898bc34ca40d7de3948f18f846
Suraj Gupta <surajgupta7070031833@gmail.com>
2025-10-25 19:35:40 +0530

    Good Problem on Simulation based and but but approach - 3 backtracing approach is owsom

--- 2048_Next_Greater_Numerically_Balanced_Number.cpp
+++ 2048_Next_Greater_Numerically_Balanced_Number.cpp
@@ -38,39 +38,80 @@ It is also the smallest numerically balanced number strictly greater than 3000.
 
 Constraints:
 
-0 <= n <= 106
+0 <= n <= 10^6
 */ 
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-class Solution {
-public:
-
-    bool balanced(int num) {
-        vector<int> freq(10);
-        while(num > 0) {
-            int digit = num%10;
-            freq[digit]++;
-            num /= 10;
-        }
-
-        for(int d = 0; d < 10; d++) {
-            if(freq[d] != 0 && freq[d] != d)
-                return false;
-        }
-        
-        return true;
-
+// Approach - 1 : Simulation
+bool balanced(int num) {
+    vector<int> freq(10);
+    while(num > 0){
+        int digit = num % 10;
+        freq[digit]++;
+        num /= 10;
     }
 
-    int nextBeautifulNumber(int n) {
-        for(int num = n+1; num <= 1224444; num++) {
-            if(balanced(num)) {
-                return num;
+    for(int d = 0; d < 10; d++){
+        if(freq[d] != 0 && freq[d] != d){
+            return false;
+        }
+    }
+    return true;
+}
+
+int nextBeautifulNumber(int n) {
+    for(int num = n+1; num <= 1224444; num++){
+        if(balanced(num)){
+            return num;
+        }
+    }
+    return -1;
+}
+
+// Approach - 2 : Upper Bound (Precomputation) -> not preferred as it requires extra space for storing precomputed values.
+
+// Approach - 3 : Backtracking (Generating all numerically balanced numbers up to 10^7)
+vector<int> digitCount = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+int backtracking(int n, int currentNum, int count) {
+    if(count == 0){
+        for(int digit=1; digit<=9; digit++){
+            if(digitCount[digit] != 0 && digitCount[digit] != digit){
+                return 0;
             }
         }
-
-        return -1;
+        return currentNum > n ? currentNum : 0;
     }
-};
+
+    int result = 0;
+    
+    for(int digit = 1; digit <= 9; digit++){
+        if(digitCount[digit] > 0 && digitCount[digit]  <= count){
+            digitCount[digit] --;
+            result = backtracking(n, currentNum * 10 + digit, count - 1);
+            digitCount[digit] ++;
+        }
+        if(result != 0){
+            break;
+        }
+    }
+    return result;
+}
+int nextBeautifulNumber(int n) {
+    int numDigits = to_string(n).length();
+    int result = backtracking(n, 0, numDigits);
+    if(result == 0){
+        result = backtracking(n, 0, numDigits + 1);
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    int result = nextBeautifulNumber(n);
+    cout << "The next greater numerically balanced number is: " << result << endl;
+    return 0;
+}
