b574bb2a98823f2843cb906e2bb2da808bae9cef
Suraj Gupta <surajgupta7070031833@gmail.com>
2025-11-02 19:49:46 +0530

    Add solution for "Count Unguarded Cells in the Grid" problem

---
+++ 2257_Count_Unguarded_Cells_Grid.cpp
@@ -0,0 +1,113 @@
+/*
+Count Unguarded Cells in the Grid - [Leetcode - 2257(Medium)]
+---------------------------------------------------------------
+You are given two integers m and n representing a 0-indexed m x n grid. You are also given two 2D integer arrays guards and walls where guards[i] = [rowi, coli] and walls[j] = [rowj, colj] represent the positions of the ith guard and jth wall respectively.
+
+A guard can see every cell in the four cardinal directions (north, east, south, or west) starting from their position unless obstructed by a wall or another guard. A cell is guarded if there is at least one guard that can see it.
+
+Return the number of unoccupied cells that are not guarded.
+
+Example 1:
+
+Input: m = 4, n = 6, guards = [[0,0],[1,1],[2,3]], walls = [[0,1],[2,2],[1,4]]
+Output: 7
+Explanation: The guarded and unguarded cells are shown in red and green respectively in the above diagram.
+There are a total of 7 unguarded cells, so we return 7.
+
+Example 2:
+
+Input: m = 3, n = 3, guards = [[1,1]], walls = [[0,1],[1,0],[2,1],[1,2]]
+Output: 4
+Explanation: The unguarded cells are shown in green in the above diagram.
+There are a total of 4 unguarded cells, so we return 4.
+
+Constraints:
+
+1 <= m, n <= 10^5
+2 <= m * n <= 10^5
+1 <= guards.length, walls.length <= 5 * 10^4
+2 <= guards.length + walls.length <= m * n
+guards[i].length == walls[j].length == 2
+0 <= rowi, rowj < m
+0 <= coli, colj < n
+All the positions in guards and walls are unique.
+*/ 
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+
+    void markGaurded(int row, int col, vector<vector<int>>& grid) {
+        //UP
+        for(int i = row-1; i >= 0; i--) {
+            if(grid[i][col] == 2 || grid[i][col] == 3) {
+                break;
+            }
+            grid[i][col] = 1; //Marking it as gaurded
+        }
+
+        //Down
+        for(int i = row+1; i < grid.size(); i++) {
+            if(grid[i][col] == 2 || grid[i][col] == 3) {
+                break;
+            }
+            grid[i][col] = 1; //Marking it as gaurded
+        }
+
+        //left
+        for(int j = col-1; j >= 0; j--) {
+            if(grid[row][j] == 2 || grid[row][j] == 3) {
+                break;
+            }
+            grid[row][j] = 1; //Marking it as gaurded
+        }
+
+        //right
+        for(int j = col+1; j < grid[0].size(); j++) {
+            if(grid[row][j] == 2 || grid[row][j] == 3) {
+                break;
+            }
+            grid[row][j] = 1; //Marking it as gaurded
+        }
+
+    }
+
+    int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
+        vector<vector<int>> grid(m, vector<int>(n, 0));
+
+        //mark gaurds positions
+        for(vector<int>& vec : guards) {
+            int i = vec[0];
+            int j = vec[1];
+            grid[i][j] = 2; //gaurd
+        }
+
+        //mark walls positions
+        for(vector<int>& vec : walls) {
+            int i = vec[0];
+            int j = vec[1];
+            grid[i][j] = 3; //wall 
+        }
+
+        for(vector<int>& gaurd : guards) {
+            int i = gaurd[0];
+            int j = gaurd[1];
+            markGaurded(i, j, grid); //four directions me check karo and mark karo gaurded
+        }
+
+        int count = 0;
+        //O(m*n)
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == 0) { //ungaurded
+                    count++;
+                }
+            }
+        }
+
+        return count;
+
+    }
+};
