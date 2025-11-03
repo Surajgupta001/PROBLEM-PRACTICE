9673ef48d66f99ef944369f6c5029dcaaacd4f26
Suraj Gupta <surajgupta7070031833@gmail.com>
2025-11-03 19:20:12 +0530

    Simulation || Prefix sum Based Problem

--- 3354_Make_Array_Elements_Equal_Zero.cpp
+++ 3354_Make_Array_Elements_Equal_Zero.cpp
@@ -15,9 +15,7 @@ Reverse your movement direction (left becomes right and vice versa).
 Take a step in your new direction.
 A selection of the initial position curr and movement direction is considered valid if every element in nums becomes 0 by the end of the process.
 
-Return the number of possible valid selections.
-
- 
+Return the number of possible valid selections. 
 
 Example 1:
 
@@ -43,8 +41,6 @@ Explanation:
 
 There are no possible valid selections.
 
- 
-
 Constraints:
 
 1 <= nums.length <= 100
@@ -57,33 +53,81 @@ There is at least one element i where nums[i] == 0.
 #include <numeric>
 using namespace std;
 
-class Solution {
-public:
-    int countValidSelections(vector<int>& nums) {
-        int n = nums.size();
-
-        int result =0;
-        int curr = 0;
-        int S = accumulate(begin(nums), end(nums), 0);//for loop bhi likh sakte ho
-
-
-        for(int i = 0; i < n; i++) {
-            curr += nums[i]; //left sum
-
-            int left  = curr;
-            int right = S - left;
-
-            if(nums[i] != 0) {
-                continue;
+//Approach-1 - (Simple simulation)
+//T.C : O(n^ * m) 
+//S.C : O(n)
+bool isValid(vector<int>& nums, int count, int i, int direction) {
+    vector<int> temp = nums;
+    
+    int idx = i;
+    
+    while (count > 0 && idx >= 0 && idx < nums.size()) {
+        if (temp[idx] > 0) {
+            temp[idx]--;
+            direction *= -1;
+            if (temp[idx] == 0) {
+                count--;
             }
-
-            if(left == right)
-                result += 2;
-            
-            if(abs(left - right) == 1)
-                result += 1;
         }
-
-        return result;
+        idx += direction;
     }
-};
+    return count == 0; //all must become 0
+}
+
+int countValidSelections(vector<int>& nums) {
+    int result = 0;
+    
+    int count = 0; //count of nums[i] != 0
+    
+    int n = nums.size();
+    
+    for (int i = 0; i < n; i++) {
+        if (nums[i] != 0) {
+            count++;
+        }
+    }
+    
+    for (int i = 0; i < n; i++) {
+        if (nums[i] == 0) {
+            if (isValid(nums, count, i, -1)) {
+                result++;
+            }
+            if (isValid(nums, count, i, 1)) {
+                result++;
+            }
+        }
+    }
+    return result;
+}
+
+//Approach-2 - (Using cumulative sum)
+//T.C : O(n) 
+//S.C : O(1)
+int countValidSelections(vector<int>& nums) {
+    int n = nums.size();
+    
+    int result =0;
+    int curr = 0;
+    
+    int S = accumulate(begin(nums), end(nums), 0);//for loop bhi likh sakte ho
+    
+    for(int i = 0; i < n; i++) {
+        curr += nums[i]; //left sum
+        
+        int left  = curr;
+        int right = S - left;
+        
+        if(nums[i] != 0) continue;
+        
+        if(left == right) result += 2;
+        
+        if(abs(left - right) == 1) result += 1;
+    }
+    return result;
+}
+
+int main() {
+    vector<int> nums = {1,0,2,0,3};
+    cout << countValidSelections(nums) << endl; // Output: 2
+    return 0;
+}
