9102423b64a5b4ff75fd8589d5372632268bd857
Suraj Gupta <surajgupta7070031833@gmail.com>
2025-11-22 19:40:39 +0530

    Good Problem based on string

--- 1930_Unique_Length-3_Palindromic_Subsequences.cpp
+++ 1930_Unique_Length-3_Palindromic_Subsequences.cpp
@@ -43,54 +43,93 @@ s consists of only lowercase English letters.
 */
 
 #include <iostream>
+#include <vector>
 #include <string>
 #include <unordered_set>
 using namespace std;
 
-class Solution
-{
-public:
-    int countPalindromicSubsequence(string s)
-    {
-        int n = s.length();
-        unordered_set<char> unique_letters;
-
-        for (char &ch : s)
-        {
-            unique_letters.insert(ch);
-        }
-
-        int result = 0;
-
-        for (char letter : unique_letters)
-        {
-
-            int first_idx = -1;
-            int last_idx = -1;
-
-            for (int i = 0; i < n; i++)
-            {
-                if (s[i] == letter)
-                {
-
-                    if (first_idx == -1)
-                    {
-                        first_idx = i;
-                    }
-
-                    last_idx = i;
-                }
-            }
-
-            unordered_set<char> st;
-            for (int middle = first_idx + 1; middle <= last_idx - 1; middle++)
-            {
-                st.insert(s[middle]);
-            }
-
-            result += st.size();
-        }
-
-        return result;
+// Approach-1
+// T.C : O(n)
+// S.C : O(1) - Only 26 sized always
+int countPalindromicSubsequence(string s) {
+    int n = s.length();
+    
+    unordered_set<char> unique_letters;
+    for (char &ch : s){
+        unique_letters.insert(ch);
     }
-};
+    
+    int result = 0;
+    
+    for (char letter : unique_letters){
+        int first_idx = -1;
+        int last_idx = -1;
+        
+        for (int i = 0; i < n; i++){
+            if (s[i] == letter){
+                if (first_idx == -1){
+                    first_idx = i;
+                }
+                last_idx = i;
+            }
+        }
+        
+        unordered_set<char> st;
+        for (int middle = first_idx + 1; middle <= last_idx - 1; middle++){
+            st.insert(s[middle]);
+        }
+        
+        result += st.size();
+    }
+    
+    return result;
+}
+
+// Approach-2 (Storing first and last indices before hand
+// T.C : O(n)
+// S.C : O(1) - Only 26 sized always
+int countPalindromicSubsequence(string s) {
+    int n = s.length();
+    
+    vector<pair<int, int>> indices(26, {-1, -1}); // Store first and last indices for each letter
+
+    for(int i = 0; i < n; i++){
+        char ch = s[i];
+        int idx = ch - 'a'; // a = 0, b = 1, ..., z = 25
+
+        if(indices[idx].first == -1){
+            indices[idx].first = i; // First occurrence
+        }
+        indices[idx].second = i; // Update last occurrence
+    }
+
+    int result = 0;
+    for (int i = 0; i < 26; i++) {
+        if (indices[i].first != -1 && indices[i].second != -1 && indices[i].second > indices[i].first) {
+            unordered_set<char> unique_middle_chars;
+            for (int j = indices[i].first + 1; j < indices[i].second; j++) {
+                unique_middle_chars.insert(s[j]);
+            }
+            result += unique_middle_chars.size();
+        }
+    }
+
+    return result;
+}
+
+
+int main() {
+    string s1 = "aabca";
+    cout << "Input: s = \"" << s1 << "\"" << endl;
+    cout << "Output: " << countPalindromicSubsequence(s1) << endl; // Expected output: 3
+
+    string s2 = "adc";
+    cout << "Input: s = \"" << s2 << "\"" << endl;
+    cout << "Output: " << countPalindromicSubsequence(s2) << endl; // Expected output: 0
+
+    string s3 = "bbcbaba";
+    cout << "Input: s = \"" << s3 << "\"" << endl;
+    cout << "Output: " << countPalindromicSubsequence(s3) << endl; // Expected output: 4
+
+    return 0;
+}
