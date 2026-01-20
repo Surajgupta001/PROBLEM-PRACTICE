bca305ca2791dec0a114b5ef88b2e8d263e227b1
Suraj Gupta <surajgupta7070031833@gmail.com>
2026-01-20 20:12:08 +0530

    Bit Manipulations based really good Problem

---
+++ 3314_Construct_Minimum_Bitwise_Array_I.cpp
@@ -0,0 +1,88 @@
+/*
+Construct the Minimum Bitwise Array I - [Leetcode - 3314(Easy)]
+------------------------------------------------------------------
+You are given an array nums consisting of n prime integers.
+
+You need to construct an array ans of length n, such that, for each index i, the bitwise OR of ans[i] and ans[i] + 1 is equal to nums[i], i.e. ans[i] OR (ans[i] + 1) == nums[i].
+
+Additionally, you must minimize each value of ans[i] in the resulting array.
+
+If it is not possible to find such a value for ans[i] that satisfies the condition, then set ans[i] = -1.
+
+Example 1:
+
+Input: nums = [2,3,5,7]
+
+Output: [-1,1,4,3]
+
+Explanation:
+
+For i = 0, as there is no value for ans[0] that satisfies ans[0] OR (ans[0] + 1) = 2, so ans[0] = -1.
+For i = 1, the smallest ans[1] that satisfies ans[1] OR (ans[1] + 1) = 3 is 1, because 1 OR (1 + 1) = 3.
+For i = 2, the smallest ans[2] that satisfies ans[2] OR (ans[2] + 1) = 5 is 4, because 4 OR (4 + 1) = 5.
+For i = 3, the smallest ans[3] that satisfies ans[3] OR (ans[3] + 1) = 7 is 3, because 3 OR (3 + 1) = 7.
+Example 2:
+
+Input: nums = [11,13,31]
+
+Output: [9,12,15]
+
+Explanation:
+
+For i = 0, the smallest ans[0] that satisfies ans[0] OR (ans[0] + 1) = 11 is 9, because 9 OR (9 + 1) = 11.
+For i = 1, the smallest ans[1] that satisfies ans[1] OR (ans[1] + 1) = 13 is 12, because 12 OR (12 + 1) = 13.
+For i = 2, the smallest ans[2] that satisfies ans[2] OR (ans[2] + 1) = 31 is 15, because 15 OR (15 + 1) = 31.
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+2 <= nums[i] <= 1000
+nums[i] is a prime number.
+*/ 
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> minBitwiseArray(vector<int>& nums) {
+    int n = nums.size();
+
+    vector<int> result(n, -1);
+    bool found = false;
+
+    for(int i = 0; i < n; i++){
+        for(int x=0; x<=nums[i]; x++){
+            if((x | (x + 1)) == nums[i]){
+                result[i] = x;
+                found = true;
+                break;
+            } else {
+                if(x == nums[i]){
+                    result[i] = -1;
+                }
+            }
+        }
+    }
+    return result;
+}
+
+int main() {
+    vector<int> nums1 = {2, 3, 5, 7};
+    vector<int> result1 = minBitwiseArray(nums1);
+    cout << "Output for nums1: ";
+    for (int num : result1) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    vector<int> nums2 = {11, 13, 31};
+    vector<int> result2 = minBitwiseArray(nums2);
+    cout << "Output for nums2: ";
+    for (int num : result2) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
