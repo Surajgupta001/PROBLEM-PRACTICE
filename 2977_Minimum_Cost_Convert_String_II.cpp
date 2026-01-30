b498359ba8817940831eeb5856d7613a1af5a416
Suraj Gupta <surajgupta7070031833@gmail.com>
2026-01-31 00:05:29 +0530

    Ultimate problem on DP and dijkstra ⭐⭐⭐⭐⭐

---
+++ 2977_Minimum_Cost_Convert_String_II.cpp
@@ -0,0 +1,169 @@
+/*
+Minimum Cost to Convert String II - [Leetcode - 2977(Hard)]
+--------------------------------------------------------------
+You are given two 0-indexed strings source and target, both of length n and consisting of lowercase English characters. You are also given two 0-indexed string arrays original and changed, and an integer array cost, where cost[i] represents the cost of converting the string original[i] to the string changed[i].
+
+You start with the string source. In one operation, you can pick a substring x from the string, and change it to y at a cost of z if there exists any index j such that cost[j] == z, original[j] == x, and changed[j] == y. You are allowed to do any number of operations, but any pair of operations must satisfy either of these two conditions:
+
+The substrings picked in the operations are source[a..b] and source[c..d] with either b < c or d < a. In other words, the indices picked in both operations are disjoint.
+The substrings picked in the operations are source[a..b] and source[c..d] with a == c and b == d. In other words, the indices picked in both operations are identical.
+Return the minimum cost to convert the string source to the string target using any number of operations. If it is impossible to convert source to target, return -1.
+
+Note that there may exist indices i, j such that original[j] == original[i] and changed[j] == changed[i].
+
+Example 1:
+
+Input: source = "abcd", target = "acbe", original = ["a","b","c","c","e","d"], changed = ["b","c","b","e","b","e"], cost = [2,5,5,1,2,20]
+Output: 28
+Explanation: To convert "abcd" to "acbe", do the following operations:
+- Change substring source[1..1] from "b" to "c" at a cost of 5.
+- Change substring source[2..2] from "c" to "e" at a cost of 1.
+- Change substring source[2..2] from "e" to "b" at a cost of 2.
+- Change substring source[3..3] from "d" to "e" at a cost of 20.
+The total cost incurred is 5 + 1 + 2 + 20 = 28. 
+It can be shown that this is the minimum possible cost.
+
+Example 2:
+
+Input: source = "abcdefgh", target = "acdeeghh", original = ["bcd","fgh","thh"], changed = ["cde","thh","ghh"], cost = [1,3,5]
+Output: 9
+Explanation: To convert "abcdefgh" to "acdeeghh", do the following operations:
+- Change substring source[1..3] from "bcd" to "cde" at a cost of 1.
+- Change substring source[5..7] from "fgh" to "thh" at a cost of 3. We can do this operation because indices [5,7] are disjoint with indices picked in the first operation.
+- Change substring source[5..7] from "thh" to "ghh" at a cost of 5. We can do this operation because indices [5,7] are disjoint with indices picked in the first operation, and identical with indices picked in the second operation.
+The total cost incurred is 1 + 3 + 5 = 9.
+It can be shown that this is the minimum possible cost.
+
+Example 3:
+
+Input: source = "abcdefgh", target = "addddddd", original = ["bcd","defgh"], changed = ["ddd","ddddd"], cost = [100,1578]
+Output: -1
+Explanation: It is impossible to convert "abcdefgh" to "addddddd".
+If you select substring source[1..3] as the first operation to change "abcdefgh" to "adddefgh", you cannot select substring source[3..7] as the second operation because it has a common index, 3, with the first operation.
+If you select substring source[3..7] as the first operation to change "abcdefgh" to "abcddddd", you cannot select substring source[1..3] as the second operation because it has a common index, 3, with the first operation.
+
+Constraints:
+
+1 <= source.length == target.length <= 1000
+source, target consist only of lowercase English characters.
+1 <= cost.length == original.length == changed.length <= 100
+1 <= original[i].length == changed[i].length <= source.length
+original[i], changed[i] consist only of lowercase English characters.
+original[i] != changed[i]
+1 <= cost[i] <= 10^6
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <set>
+#include <queue>
+#include <climits>
+using namespace std;
+
+long long dijkstra(string &startStr, string &endStr, unordered_map<string, vector<pair<string, long long>>> &graph) {
+    
+    // Min-Heap Priority Queue
+    priority_queue<pair<long long, string>, vector<pair<long long, string>>, greater<pair<long long, string>>> pq; // { cost, string }
+
+    unordered_map<string, long long> result; // { string : minCost }
+    result[startStr] = 0;
+    pq.push({0, startStr});
+
+    while(!pq.empty()){
+        long long currCost = pq.top().first;
+        string node = pq.top().second;
+        pq.pop();
+
+        if(node == endStr){
+            break;
+        }
+
+        for(auto &neighborPair : graph[node]){
+            string neighborNode = neighborPair.first;
+            long long edgeCost = neighborPair.second;
+
+            if(!result.count(neighborNode) || currCost + edgeCost < result[neighborNode]){
+                result[neighborNode] = currCost + edgeCost;
+                pq.push({result[neighborNode], neighborNode});
+            }
+        }
+    }
+    return result.count(endStr) ? result[endStr] : 1e10;
+}
+
+long long helper(int idx, vector<long long> &dp, string &sourceStr, string &targetStr, set<int> &validLengths, unordered_map<string, vector<pair<string, long long>>> &graph) {
+    if(idx >= sourceStr.size()){
+        return 0;
+    }
+
+    if(dp[idx] != -1){
+        return dp[idx];
+    }
+
+    long long minCost = 1e10;
+
+    if(sourceStr[idx] == targetStr[idx]){
+        minCost = helper(idx + 1, dp, sourceStr, targetStr, validLengths, graph);
+    }
+
+    for(auto &length : validLengths){
+        
+        if(idx + length > sourceStr.size()){
+            break;
+        }
+
+        string subSourceStr = sourceStr.substr(idx, length);
+        string subTargetStr = targetStr.substr(idx, length);
+
+        if(!graph.count(subSourceStr)){
+            continue;
+        }
+
+        long long minPathCost = dijkstra(subSourceStr, subTargetStr, graph);
+        if(minPathCost == 1e10){
+            continue;
+        }
+
+        minCost = min(minCost, minPathCost + helper(idx + length, dp, sourceStr, targetStr, validLengths, graph));
+    }
+
+    return dp[idx] = minCost;
+}
+
+
+long long minimumCost(string source, string target, vector<string>& original, vector<string>& changed, vector<int>& cost) {
+    int n = source.size();
+
+    unordered_map<string, vector<pair<string, long long>>> graph; // { string : { (neighbor, weight) } }
+    set<int> validLengths;
+    vector<long long> dp(n + 1, -1);
+
+    // Build the transformation graph
+    for(int i = 0; i < (int)original.size(); i++) {
+        graph[original[i]].push_back({changed[i], cost[i]});
+    }
+
+    for(auto &subString : original){
+        validLengths.insert((int)subString.size());
+    }
+
+    long long result = helper(0, dp, source, target, validLengths, graph);
+
+    return result == 1e10 ? -1 : result;
+}
+
+int main() {
+    string source = "abcd";
+    string target = "acbe";
+    vector<string> original = {"a","b","c","c","e","d"};
+    vector<string> changed = {"b","c","b","e","b","e"};
+    vector<int> cost = {2,5,5,1,2,20};
+
+    long long result = minimumCost(source, target, original, changed, cost);
+
+    cout << "Minimum Cost to Convert String II: " << result << endl;
+
+    return 0;
+}
