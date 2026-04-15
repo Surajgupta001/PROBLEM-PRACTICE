54a751c52a27e1e007c85a4fa558dbfd27c428d8
Suraj Gupta <surajgupta7070031833@gmail.com>
2026-04-15 19:33:46 +0530

    Straight Logic Based Question on circular string array

---
+++ 2515_Shortest_Distance_Target_String_Circular_Array.cpp
@@ -0,0 +1,75 @@
+/*
+Shortest Distance to Target String in a Circular Array - [Leetcode - 2515(Easy)]
+----------------------------------------------------------------------------------
+You are given a 0-indexed circular string array words and a string target. A circular array means that the array's end connects to the array's beginning.
+
+Formally, the next element of words[i] is words[(i + 1) % n] and the previous element of words[i] is words[(i - 1 + n) % n], where n is the length of words.
+Starting from startIndex, you can move to either the next word or the previous word with 1 step at a time.
+
+Return the shortest distance needed to reach the string target. If the string target does not exist in words, return -1.
+
+Example 1:
+
+Input: words = ["hello","i","am","leetcode","hello"], target = "hello", startIndex = 1
+Output: 1
+Explanation: We start from index 1 and can reach "hello" by
+- moving 3 units to the right to reach index 4.
+- moving 2 units to the left to reach index 4.
+- moving 4 units to the right to reach index 0.
+- moving 1 unit to the left to reach index 0.
+The shortest distance to reach "hello" is 1.
+
+Example 2:
+
+Input: words = ["a","b","leetcode"], target = "leetcode", startIndex = 0
+Output: 1
+Explanation: We start from index 0 and can reach "leetcode" by
+- moving 2 units to the right to reach index 2.
+- moving 1 unit to the left to reach index 2.
+The shortest distance to reach "leetcode" is 1.
+
+Example 3:
+
+Input: words = ["i","eat","leetcode"], target = "ate", startIndex = 0
+Output: -1
+Explanation: Since "ate" does not exist in words, we return -1.
+
+Constraints:
+
+1 <= words.length <= 100
+1 <= words[i].length <= 100
+words[i] and target consist of only lowercase English letters.
+0 <= startIndex < words.length
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <climits>
+#include <algorithm>
+using namespace std;
+
+int closestTarget(vector<string>& words, string target, int startIndex) {
+    int n = words.size();
+    int minDistance = INT_MAX;
+
+    for(int i=0; i<n; i++){
+        if(words[i] == target){
+            int straightDistance = abs(i - startIndex);
+            int circularDistance = n - straightDistance;
+
+            minDistance = min({minDistance, straightDistance, circularDistance});
+        }
+    }
+    return (minDistance == INT_MAX) ? -1 : minDistance;
+}
+
+int main () {
+    vector<string> words1 = {"hello","i","am","leetcode","hello"};
+    string target1 = "hello";
+    int startIndex1 = 1;
+    
+    cout << closestTarget(words1, target1, startIndex1) << endl; // 1
+
+    return 0;
+}
