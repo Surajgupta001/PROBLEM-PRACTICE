48dab3f51501c9abb4e77e7b6a25f11667aeb67d
surajgupta001 <surajgupta7070031833@gmail.com>
2026-07-17 19:42:44 +0530

    String two pointer approach

--- 1358_Number_Substrings_Containing_All_Three_Characters.cpp
+++ 1358_Number_Substrings_Containing_All_Three_Characters.cpp
@@ -1,6 +1,6 @@
 /*
 Number of Substrings Containing All Three Characters - [Leetcode - 1358(Medium)]
-===============================================================================
+========================================================================================
 Given a string s consisting only of characters a, b and c.
 
 Return the number of substrings containing at least one occurrence of all these characters a, b and c.
@@ -31,10 +31,31 @@ s only consists of a, b or c characters.
 
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
 
+// Sliding window approach
 int numberOfSubstrings(string s){
-    
+    int n = s.length();
+    vector<int> count(3, 0); // Count of 0 -> 'a', 1 ->'b', 2 -> 'c'
+
+    int i = 0;
+    int j = 0;
+
+    int result = 0;
+
+    while(j < n){
+        count[s[j] - 'a']++; // Increment the count of the current character
+
+        // Check if we have at least one of each character
+        while(count[0] > 0 && count[1] > 0 && count[2] > 0){
+            result += (n - j); // All substrings starting from i to j are valid
+            count[s[i] - 'a']--; // Move i pointer to reduce the window size
+            i++;
+        }
+        j++;
+    }
+    return result;
 }
 
 int main(){
