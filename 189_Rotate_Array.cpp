aa80477a615a05211410d749ffe82a40ea090aa2
surajgupta001 <surajgupta7070031833@gmail.com>
2026-07-20 16:29:36 +0530

    Rotated Array with stl - smart approach

---
+++ 189_Rotate_Array.cpp
@@ -0,0 +1,59 @@
+/*
+Rotate Array - [Leetcode - 189(Medium)]
+-------------------------------------------
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+Example 2:
+
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100] 
+
+Constraints:
+
+1 <= nums.length <= 10^5
+-2^31 <= nums[i] <= 2^31 - 1
+0 <= k <= 10^5
+ 
+
+Follow up:
+
+Try to come up with as many solutions as you can. There are at least three different ways to solve this problem.
+Could you do it in-place with O(1) extra space?
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void rotate(vector<int>& nums, int k) {
+    int n = nums.size();
+    k = k % n; // In case k is greater than n
+    if (k == 0) return; // No need to rotate if k is 0
+    reverse(nums.begin(), nums.end());
+    reverse(nums.begin(), nums.begin() + k);
+    reverse(nums.begin() + k, nums.end());
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3, 4, 5, 6, 7};
+    int k = 3;
+    rotate(nums, k);
+    for (int num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+    return 0;
+}
